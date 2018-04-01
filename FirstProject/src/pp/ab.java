cc6dd6886403810135431304ee826d9755a30dc0
Hemesh <Hemesh@Hemesh-PC>
2018-04-01 17:19:59 +0530

    checking the debugging env

--- FirstProject/src/pp/ab.java
+++ FirstProject/src/pp/ab.java
@@ -1,5 +1,16 @@
 package pp;
 
 public class ab {
-
+	static int value=0;
+	static int value1=0;
+	
+	public static void test(int val){
+		value=value1+val;
+		System.out.println(value);
+	}
+public static void main(String[] args){
+	System.out.println("test");
+	System.out.println("test2");
+	test(4);
+}
 }
